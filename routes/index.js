83da7b405bcc4fa6e047f545601cabe35f6e1453
Daniel-Loaiza <danielloaizaisi@gmail.com>
2023-06-04 22:24:20 -0500

    modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
+const express = require('express');
 const moviesRouter = require('./moviesRouter');
 const commentsRouter = require('./commentsRouter');
 
 function routerApi(app){
+    const router = express.Router();
+    app.use('/api/v1', router);
     app.use('/movies', moviesRouter);
     app.use('/comments', commentsRouter);       
 }
